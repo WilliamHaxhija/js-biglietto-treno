fa4482cb74c3a5892a3aeef05662b9ff12574682
WilliamHaxhija <williamhaxhija@gmail.com>
2024-03-05 14:36:50 +0100

    feat: add problem list

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// INFO DA OTTENERE
+  //Numero km che vuole percorrere
+  //Età passeggero
+//FUNZIONE PROGRAMMA
+  //Calcolo prezzo totale del viaggio
+    //Prezzo biglietto: 0.21$/km
+    //Sconto 20% minorenni, sconto 40% over 65
+//OUTPUT
+  //Prezzo con massimo i centesimi
